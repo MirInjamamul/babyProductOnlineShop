fc8b738eeaed5f185e20e9667044753efee30fab
MirInjamamul <injamul120221@gmail.com>
2019-12-24 03:31:59 +0600

    login and signup complete

---
+++ index.php
@@ -0,0 +1,87 @@
+<?php
+session_start();
+
+// initializing variables
+$username = "";
+$email = "";
+$phone_no = "";
+$password = "";
+
+// connect to the database
+$db = mysqli_connect('localhost', 'root', '', 'baby');
+// REGISTER USER
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // receive all input values from the form
+  $username = mysqli_real_escape_string($db, $_POST['username']);
+  $email = mysqli_real_escape_string($db, $_POST['email']);
+  $phone_no = mysqli_real_escape_string($db, $_POST['phone_no']);
+  $password = mysqli_real_escape_string($db, $_POST['password']);
+
+  	$query = "INSERT INTO info (username, email, phone_no, password)
+  			  VALUES('$username', '$email', '$phone_no', '$password')";
+  	mysqli_query($db, $query);
+  	$_SESSION['username'] = $username;
+  	//$_SESSION['success'] = "You are now logged in";
+  	header('location: login.php');
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sign Up Form</title>
+
+    <!-- Font Icon -->
+    <link rel="stylesheet" href="fonts/material-icon/css/material-design-iconic-font.min.css">
+
+    <!-- Main css -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+    <div class="main">
+
+        <!-- Sign up form -->
+        <section class="signup">
+            <div class="container">
+                <div class="signup-content">
+                    <div class="signup-form">
+                        <h2 class="form-title">Sign up</h2>
+                        <form action="#" method="POST">
+                            <div class="form-group">
+                                <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                                <input type="text" name="username" id="name" placeholder="Your Name"/>
+                            </div>
+                            <div class="form-group">
+                                <label for="email"><i class="zmdi zmdi-email"></i></label>
+                                <input type="email" name="email" id="email" placeholder="Your Email"/>
+                            </div>
+			    <div class="form-group">
+                                <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                                <input type="text" name="phone_no" id="name" placeholder="Your Phone Number"/>
+                            </div>
+                            <div class="form-group">
+                                <label for="pass"><i class="zmdi zmdi-lock"></i></label>
+                                <input type="password" name="password" id="pass" placeholder="Password"/>
+                            </div>                                                       
+                            <div class="form-group form-button">
+                                <input type="submit" name="signup" id="signup" class="form-submit" value="Register"/>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="signup-image">
+                        <figure><img src="images/signup-image.jpg" alt="sing up image"></figure>
+                        <a href="login.php" class="signup-image-link">I am already member</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+
+    <!-- JS -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
